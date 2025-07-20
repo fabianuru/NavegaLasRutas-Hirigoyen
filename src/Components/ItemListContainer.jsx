55b9c31ffe0a7c4676f9beebae2f1f60ed581a95
HiruFabi <fhirigoyenproy@gmail.com>
2025-07-20 18:58:47 -0300

    Productos renderizados

--- src/Components/ItemListContainer.jsx
+++ src/Components/ItemListContainer.jsx
@@ -4,14 +4,12 @@ const ItemListContainer = ({welcome}) => {
 
 const [products, setProducts] = useState([])
 
-useEffect(()=> {
-  fetch("https://dummyjson.com/products")
-  .then((response)=> {return response.json()})
-  .then((products)=> {
-    setProducts(products)
-  })
-  
-},[])
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data.products)) // ¡Ojo! la propiedad es `data.products`
+      .catch((error) => console.error("Error al cargar productos:", error));
+  }, []);
 
 
   return (
@@ -20,8 +18,15 @@ useEffect(()=> {
     
     className="main">{welcome}
 
-    <div>
-      {JSON.stringify(products)}
+     <div className="product-grid">
+      {products.map((product) => (
+        <div className="product-card" key={product.id}>
+          <img src={product.thumbnail} alt={product.title} />
+          <h3>{product.title}</h3>
+          <p>{product.description}</p>
+          <p><strong>${product.price}</strong></p>
+        </div>
+      ))}
     </div>
 
 
